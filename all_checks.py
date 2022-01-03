4381ec7a540f02bac8667afaa98b2b51f8fe5e0e
LucianT93 <lucianlukas93@gmail.com>
2022-01-03 17:25:51 +0200

    Reordered conditional to match parameter order

--- all_checks.py
+++ all_checks.py
@@ -13,7 +13,7 @@ def check_disk_full(disk,min_absolute,min_percent):
     du=shutil.disk_usage(disk)
     percent_free=100*du.free/du.total
     gigabytes_free=du.free/2**30
-    if percent_free<min_percent or gigabytes_free<min_absolute:
+    if gigabytes_free<min_absolute or percent_free<min_percent:
         return True
     return False
 
